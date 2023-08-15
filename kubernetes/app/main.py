cbb234a14a5b6237e9db67d9f47de92cdbcd35ee
chandrurnaik <137071474+chandrurnaik@users.noreply.github.com>
2023-08-15 18:04:21 +0530

    Create main.py

---
+++ kubernetes/app/main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import socket, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def print_ip():
+    hostname = socket.gethostname()
+    get_ip = socket.hostbyname(hostname)
+    return get_ip
+
+@app.route('/health')
+def check_health():
+    return json.dumps({'success':True}, 200, {'contentType': 'application/json'})
+
+@app.route('/name')
+def print_name():
+    return 'Chandrashekhar R'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8080')
